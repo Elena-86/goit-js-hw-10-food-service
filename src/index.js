bae88046a6c892a7fb4e7f4fec5e7052213023f6
Tiutiunnyk Elena <elena.tiutiunnyk@gmail.com>
2021-05-23 17:37:24 +0300

    home work 10

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import './styles.css';
+
+import './index.html';
 import './menu.json';
+import './styles.css';
+import './menu-creator';
+import './theme-switcher';
+import './templates/menu-template.hbs';
